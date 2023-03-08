7f2130547dc56ee951cbadd3ce92c8bc337f27b6
Sergio Andres Garcia <sergioandresgarcia011@gmail.com>
2023-03-08 18:00:16 -0300

    services card

---
+++ 1)_Agencia_de_Marketing/src/components/services/ServicesCard.js
@@ -0,0 +1,8 @@
+function ServicesCard(){
+    return(
+        <div className="w-full h-96 lg:h-[20pm] bg-white shadow-xl hover:-translate-y-1 duration-300 ease-in-out">
+            ServicesCard
+        </div>
+    )
+}
+export default ServicesCard
